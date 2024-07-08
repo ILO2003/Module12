4b5a5e34870eb54f9b1f0e82f2d1bc09e60e52e7
Ilia Javakhishvili <ilo@Ilias-iMac.local>
2024-07-09 00:29:01 +0200

    add ec2 deployment configuration

--- main.tf
+++ main.tf
@@ -2,32 +2,127 @@ provider "aws" {
     region = "eu-north-1"
 }
 
-resource "aws_vpc" "development-vpc-ilo" {
-    cidr_block = "10.0.0.0/16"
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip{}
+variable instance_type{}
+variable public_key{}
+
+resource "aws_vpc" "myapp-vpc-ilo" {
+    cidr_block = var.vpc_cidr_block
     tags = {
-      Name: "development",
-      vpc_env: "dev"
+      Name: "${var.env_prefix}-vpc-ilo"
     }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.development-vpc-ilo.id
-    cidr_block = "10.0.15.0/24"
-    availability_zone = "eu-north-1a"
+resource "aws_subnet" "myapp-subnet-1-ilo" {
+    vpc_id = aws_vpc.myapp-vpc-ilo.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
     tags = {
-      Name: "subnet-1-dev"
+      Name: "${var.env_prefix}-subnet-1-ilo"
     }
 }
 
-data "aws_vpc" "existing_vpc" {
-    default = true
+resource "aws_internet_gateway" "myapp-igw-ilo"{
+    vpc_id = aws_vpc.myapp-vpc-ilo.id
+    tags = {
+        Name: "${var.env_prefix}-igw-ilo"
+    }
 }
 
-resource "aws_subnet" "dev-subnet-2" {
-    vpc_id = data.aws_vpc.existing_vpc.id
-    cidr_block = "172.31.48.0/20"
-    availability_zone = "eu-north-1a"
-    tags = {
-      Name: "subnet-2-default"
+resource "aws_route_table" "myapp-route-table-ilo"{
+    vpc_id = aws_vpc.myapp-vpc-ilo.id
+
+    route{
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw-ilo.id
     }
+    tags = {
+        Name: "${var.env_prefix}-rtb-ilo"
+    }
+}
+
+resource "aws_route_table_association" "a-rtb-subnet-ilo"{
+    subnet_id = aws_subnet.myapp-subnet-1-ilo.id
+    route_table_id = aws_route_table.myapp-route-table-ilo.id
+}
+
+resource "aws_security_group" "myapp-sg-ilo"{
+    name = "myapp-sg-ilo"
+    vpc_id = aws_vpc.myapp-vpc-ilo.id
+
+    ingress{
+        from_port = 22
+        to_port = 22
+        protocol = "TCP"
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress{
+        from_port = 8080
+        to_port = 8080
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress{
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-sg-ilo"
+    }
+}
+data "aws_ami" "latest-amazon-linux-image"{
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+resource "aws_key_pair" "ssh-key"{
+    key_name = "server-key"
+    public_key = var.public_key
+}
+
+resource "aws_instance" "myapp-server-ilo"{
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1-ilo.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg-ilo.id]
+    availability_zone = var.avail_zone
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+
+    user_data = <<E0F
+                    #!/bin/bash
+                    sudo yum update -y && sudo yum install -y docker
+                    sudo systemctl start docker
+                    sudo usermod -aG docker ec2-user
+                    docker run -p 8080:80 nginx
+
+                E0F
+    user_data_replace_on_change = true
+
+    tags = {
+        Name: "${var.env_prefix}-server"
+    }
+}
+
+output "ec2_public_ip"{
+    value = aws_instance.myapp-server-ilo.public_ip
 }
