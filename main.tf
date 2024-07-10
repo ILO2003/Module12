a619421b91a14d905acc29d483a37743124f469e
Ilia Javakhishvili <ilo@Ilias-iMac.local>
2024-07-10 23:44:47 +0200

    provisioner and connection attributes, initial push

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ variable env_prefix {}
 variable my_ip{}
 variable instance_type{}
 variable public_key{}
+variable private_key{}
+
 
 resource "aws_vpc" "myapp-vpc-ilo" {
     cidr_block = var.vpc_cidr_block
@@ -108,15 +110,38 @@ resource "aws_instance" "myapp-server-ilo"{
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
-    user_data = <<E0F
-                    #!/bin/bash
-                    sudo yum update -y && sudo yum install -y docker
-                    sudo systemctl start docker
-                    sudo usermod -aG docker ec2-user
-                    docker run -p 8080:80 nginx
+    # user_data = <<E0F
+     #               #!/bin/bash
+      #              sudo yum update -y && sudo yum install -y docker
+       #             sudo systemctl start docker
+        #            sudo usermod -aG docker ec2-user
+         #           docker run -p 8080:80 nginx
 
-                E0F
+          #      E0F
     user_data_replace_on_change = true
+
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ec2-user"
+        private_key = file(var.private_key)
+    }
+
+    provisioner "file" {
+        source = "source_of_file.sh"
+        destination "/on/the/resource/filename.sh"
+    }
+
+    provisioner "remote-exec"{
+        inline = [
+            "export ENV=dev",
+            "mkdir new dir"
+        ]
+    }
+
+    provisioner "local-exec"{
+        command = "echo ${self.public_ip} > output.txt"
+    }
 
     tags = {
         Name: "${var.env_prefix}-server"
