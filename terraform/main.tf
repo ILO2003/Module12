dc036ce8dccf6df83d4f000701f7b1e82551fab4
Ilia Javakhishvili <ilo@Ilias-iMac.local>
2024-07-12 23:41:31 +0200

    added s3 bucket, tf remote backend configured

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform{
+    required_version = ">= 0.12"
+    backend "s3" {
+        bucket = "myapp-tf-s3-bucket-ilo"
+        key = "myapp/state.tfstate"
+        region = "eu-north-1" 
+    }
+}
+
 provider "aws" {
     region = var.region
 }
